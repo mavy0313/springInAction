91f415948b98558e8b9a0365fbdaba41e655199a
mavy0313 <vypoff@gmail.com>
2019-01-29 00:46:23 +0400

    First commit

---
+++ src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -0,0 +1,20 @@
+package spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+
+}
